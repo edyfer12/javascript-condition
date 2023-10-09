d096661a3f4849e2e13033bfdc3728b892ebfb58
Edin <edyfer2@live.com.au>
2023-10-09 10:23:54 +0200

    Create an if statement

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 let hour = new Date().getHours();
 
 //Create an if statement
+
+if ( hour > 18) {
+
+    //Display the message
+    
+    document.getElementById('#greeting').innerHTML = 
+    "Good evening";
+}
